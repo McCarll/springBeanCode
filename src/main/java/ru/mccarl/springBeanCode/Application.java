6790ef500a455548a9e4f4e9027f9ab596782602
Vasiliy Rudometkin <vrudometkin@alfabank.ru>
2017-11-24 00:46:34 +0300

    fix component

--- src/main/java/ru/mccarl/springBeanCode/Application.java
+++ src/main/java/ru/mccarl/springBeanCode/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
-        "ru.springJustCode"})
+        "ru.mccarl.springBeanCode"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
